1f57c50c2e85d90844917d401657eba98e728c9e
Mike C <mikecronin92@gmail.com>
2018-04-02 02:48:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+require 'pry'
 def display_board(board)
   cells = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
   cells.map! {|xo| cells[xo][1] = board[xo.index(xo)]}
+  binding.pry
 end
 
 board = [" "," "," "," "," "," "," "," "," "]
