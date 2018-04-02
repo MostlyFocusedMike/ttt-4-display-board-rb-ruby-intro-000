43ad1f480522947a2edd628784f982151c93239e
Mike C <mikecronin92@gmail.com>
2018-04-02 03:04:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,6 @@
 # out the current state.
 require 'pry'
 def display_board(board)
-  cells = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
-  7.times {|i| cells[i][1] = board[i]}
-  # Would Print:
   puts " #{board[0]} | #{board[1]} | #{board[2]} "  
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "    
